fabc04663a52d82515cbbbc2debe6b46dad29e83
Tristan Slater <hello@trslater.ca>
2022-05-24 15:14:24 -0700

    Start subset problem from 3b1b

---
+++ subset_problem.py
@@ -0,0 +1,15 @@
+"""Number of Subsets w/ Sums Divisible by n
+
+This is a generalization of a problem discussed in ["The unreasonable
+effectiveness of complex numbers in discrete math"](
+    https://www.youtube.com/watch?v=bOXCLR3Wric&t=23s), a 3Blue1Brown video.
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
