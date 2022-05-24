0ce1880dc02e19aa1e2b730dcec90baadc021c6d
Tristan Slater <hello@trslater.ca>
2022-05-24 15:21:23 -0700

    Complete basic brute force solution

--- subset_problem.py
+++ subset_problem.py
@@ -1,4 +1,4 @@
-"""Number of Subsets w/ Sums Divisible by n
+"""Number of Subsets w/ Sums Divisible by k
 
 This is a generalization of a problem discussed in ["The unreasonable
 effectiveness of complex numbers in discrete math"](
@@ -7,8 +7,22 @@ effectiveness of complex numbers in discrete math"](
 """
 
 
+from itertools import chain, combinations
+import sys
+
+
 def main():
-    pass
+    n, k = map(int, sys.argv[1:3])
+
+    s = set(range(1, n + 1))
+
+    p = powerset(s)
+
+    print(sum(1 for ss in p if sum(ss) % k == 0))
+
+
+def powerset(s: set) -> set:
+    return set(chain.from_iterable(combinations(s, r) for r in range(len(s) + 1)))
 
 
 if __name__ == "__main__":
